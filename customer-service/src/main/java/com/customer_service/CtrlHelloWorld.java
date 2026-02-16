3a9b958957b584e7d0e344827c53919a89adfa12
Jesús Ivan Saavedra Martinez <isaavedra@grupopm.mx>
2026-02-16 07:52:23 -0600

    first commit

---
+++ customer-service/src/main/java/com/customer_service/CtrlHelloWorld.java
@@ -0,0 +1,21 @@
+package com.customer_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class CtrlHelloWorld {
+
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+	@GetMapping
+	public String helloWorld2() {
+		return "Hello World 2";
+	}
+
+}
