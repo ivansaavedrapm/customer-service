0c54da904dfc166d5466b1b18b7e6438d5271431
Jesús Ivan Saavedra Martinez <isaavedra@grupopm.mx>
2026-02-25 07:56:48 -0600

    feature region repository

---
+++ customer-service/src/main/java/com/customer_service/api/repository/RepoRegion.java
@@ -0,0 +1,17 @@
+package com.customer_service.api.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.customer_service.api.entity.Region;
+
+@Repository
+public interface RepoRegion extends JpaRepository<Region, Integer>{
+
+	@Query(value ="SELECT * FROM region ORDER BY region", nativeQuery = true)
+	List<Region> getRegions();
+
+}
