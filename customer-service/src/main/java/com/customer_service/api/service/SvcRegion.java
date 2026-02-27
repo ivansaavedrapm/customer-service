73a3ffbeaa962bce772d08eaa05a01b662216d64
Jesús Ivan Saavedra Martinez <isaavedra@grupopm.mx>
2026-02-27 13:50:13 -0600

    feature region service

---
+++ customer-service/src/main/java/com/customer_service/api/service/SvcRegion.java
@@ -0,0 +1,10 @@
+package com.customer_service.api.service;
+
+import java.util.List;
+
+import com.customer_service.api.entity.Region;
+
+public interface SvcRegion {
+
+	public List<Region> getRegions();
+}
