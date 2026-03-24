265a4a456aeb1bfbe325929eadda7cf570789236
Jesús Ivan Saavedra Martinez <isaavedra@grupopm.mx>
2026-03-24 10:55:33 -0600

    feature customer entity, dto & repository

---
+++ customer-service/src/main/java/com/customer_service/api/service/SvcCustomer.java
@@ -0,0 +1,18 @@
+package com.customer_service.api.service;
+
+import java.util.List;
+
+import com.customer_service.api.dto.DtoCustomerIn;
+import com.customer_service.api.dto.DtoCustomerListOut;
+
+public interface SvcCustomer {
+
+	public List<DtoCustomerListOut> findAll();
+	public DtoCustomerOut findById(Integer id);
+	
+	public void create(DtoCustomerIn in);
+	public void update(Integer id, DtoCustomerIn in);
+	
+	public void enable(Integer id);
+	public void disable(Integer id);
+}
