0c033066fce79d52a181fbb738db3c1357ad155e
Iván Saavedra <ivan.saavedra@people-media.com.mx>
2026-03-31 18:12:56 -0600

    feature upload customer image

---
+++ customer-service/src/main/java/com/customer_service/api/repository/RepoCustomerImage.java
@@ -0,0 +1,11 @@
+package com.customer_service.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.customer_service.api.entity.CustomerImage;
+
+@Repository
+public interface RepoCustomerImage extends JpaRepository<CustomerImage, Integer> {
+
+}
